a666cd82b7981de4b07a8259dc36363571b66e39
Tsubasa <wing@me.wing.osaka>
2025-03-16 22:51:36 +0900

    helmet_home

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,10 @@ import { Helmet } from 'react-helmet'; // Helmet
 function Home() {
   return (
     <>
+      <Helmet>
+        <title>Wingのサイト</title>
+        <meta name="description" content="Wingのサイト．地球在住．もしかしたら，大阪に住んでるかもしれません（知らんけど）．気になったら，ウェブサイト見てな．"/>
+      </Helmet>
       <h2>Home</h2>
     </>
   )
