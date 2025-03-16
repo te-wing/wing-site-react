60fe9a3f043844290a3e8f6df5f76b865e18d39c
Tsubasa <wing@me.wing.osaka>
2025-03-16 18:56:34 +0900

    redirect

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,3 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import { Link, Routes, Route } from 'react-router-dom';  // Router
 
@@ -18,8 +15,6 @@ function Contact() {
 }
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <div>
