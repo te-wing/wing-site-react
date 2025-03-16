2b33958ae0d579e05629845318637564e1fcd3e1
Tsubasa <wing@me.wing.osaka>
2025-03-16 18:33:18 +0900

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,8 @@ import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';  // Linkをインポート
-import Home from './Home';
-import About from './About';
+import Home from './home';
+import About from './about';
 
 /* Routerの中身 */
 const Home =() => {
