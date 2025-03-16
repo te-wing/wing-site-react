a8ca77879c048d294b0cbd4da9958510d298b3d3
Tsubasa <wing@me.wing.osaka>
2025-03-16 18:34:09 +0900

    delete

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import './App.css';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';  // Linkをインポート
 import Home from './home';
