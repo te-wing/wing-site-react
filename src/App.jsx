fb8a1a2f531c2e0f5d55cfca9958ffadba899f80
Tsubasa <wing@me.wing.osaka>
2025-03-21 22:45:50 +0900

    download

--- src/App.jsx
+++ src/App.jsx
@@ -4,23 +4,9 @@ import { Helmet } from 'react-helmet'; // Helmet
 import React, { Suspense, lazy } from 'react'; //分割
 import Home from './pages/Home';
 import About from './pages/About';
+import Download from './pages/Download';
 
 //　ページコンポーネント
-
-function Download() {
-  return(
-    <>
-      <Helmet>
-        <title>Downloads | Wingのサイト</title>
-        <meta name="description" content="このサイトに掲載している一部コンテンツ（ロゴなど）がダウンロードできます．一部のコンテンツには，個別のダウンロードリンクが用意されている場合があります．いずれのコンテンツも，無許可での転載・改変はご遠慮ください．" />
-      </Helmet>
-      <h2>Downloads</h2>
-      <p className="text">下記リンクをクリックして，ダウンロード可能です．個別の記載があるものを除き，無許可での転載・改変は禁じます．転載・改変を希望される場合は，Contactsに記載の連絡先までご連絡ください．尚，正当な手続きを経て転載される場合も，出典を明記してご利用ください．</p>
-      <a href="./wingLogo.png" download='wingLogo.png'>このサイトのロゴ（PNG）</a>
-      <a href="./wingLogo.svg" download='wingLogo.svg'>このサイトのロゴ（SVG）</a>
-    </>
-  )
-}
 
 function Contact() {
   return (
