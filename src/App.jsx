24c1eaa13cf86d49220f6aec7a181ff8df173b79
Tsubasa <wing@me.wing.osaka>
2025-03-21 22:42:14 +0900

    about

--- src/App.jsx
+++ src/App.jsx
@@ -3,30 +3,9 @@ import { Link, Routes, Route } from 'react-router-dom';  // Router
 import { Helmet } from 'react-helmet'; // Helmet
 import React, { Suspense, lazy } from 'react'; //分割
 import Home from './pages/Home';
+import About from './pages/About';
 
 //　ページコンポーネント
-
-function About() {
-  return (
-    <>
-      <Helmet>
-        <title>About Me | Wingのサイト</title>
-        <meta name="description" content="Who am I? 私は，地球のどこかに住む地球人であります（なんとか構文！！）．もしかして，あなたは私と既に出会ったことがあるかもしれません（知らんけど）．趣味は，写真，プログラミング，音楽など．" />
-      </Helmet>
-      <h2>About Me</h2>
-      <h3>自己紹介</h3>
-      <p className="text">地球のどこかに住む地球人です（小泉構文！！）．現在は，地球のどこかで高校に通っています．あなたとはもしかすると，もう私と会ったことがあるかもしれません（知らんけど）...</p>
-      <p className="text">このサイトは，まだまだ作成途中なので，これから色々追加していこうと思います．ぜひ今後もご覧ください．</p>
-      <p className="text">私の正体が気になる人は，下記のメールアドレスに連絡してみてください．反応するかもしれません．（知らんけど）</p>
-      <h3>趣味</h3>
-      <p className="text">
-      写真，プログラミング，音楽など．SONY α6000で写真を撮ってるので，自分が撮った写真はそのうち，このサイトにも載せようと思ってます．</p>
-      <p className="text">プログラミングに関しては，もともとはMacでSwiftをいじって全く意味あらへんiPhoneアプリとかを作ってたんやけど，最近になってウェブ開発に興味が出始めたのは，友人に勧められて始めて見たからです．GitHubプロフィールを載せていますので，そのうちソースコードを公開するかもしれません...</p>
-      <p className="text">プログラミングに関しては，もともとはMacでSwiftをいじって全く意味あらへんiPhoneアプリとかを作ってたんやけど，最近になってウェブ開発に興味が出始めたのは，友人に勧められて始めて見たからです．GitHubプロフィールを載せていますので，そのうちソースコードを公開するかもしれません...</p>
-      <p className="text">音楽．自分は昔ピアノを習ってましたが，いつのまにか辞めてしまいました．一方で，最近歌を歌うことの気持ちよさに気づいて，学校で歌ったりしてますがますが，権利の関係とかがややこしいので，しばらくはこのサイトで公開することはないでしょう...</p>
-    </>
-  )
-}
 
 function Download() {
   return(
