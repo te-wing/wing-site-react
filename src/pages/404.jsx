43bab13a91698de34e4073332d013264bbea5ab2
wing-0902 <wing@me.wing.osaka>
2025-03-30 16:42:57 +0900

    404.jsx を更新

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,1 +1,14 @@
-a
+import { Helmet } from 'react-helmet'; // Helmet
+
+export default function Error404() {
+    return (
+        <>
+            <Helmet>
+                <title>404 Not Found | Wingのサイト</title>
+                <meta name="description" content="お探しのページは見つかりませんでした．削除されたか，移動した可能性があります．" />
+            </Helmet>
+            <h2>404 Nof Found</h2>
+            <p>お探しのページは見つかりませんでした．削除されたか，移動した可能性があります．</p>
+        </>
+    );
+}
