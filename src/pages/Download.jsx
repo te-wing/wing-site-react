bd3fe06984d675dc39226ee9e84ebbc027d78d70
wing-0902 <114337707+te-wing@users.noreply.github.com>
2025-04-01 16:13:49 +0900

    Download.jsx を更新

--- src/pages/Download.jsx
+++ src/pages/Download.jsx
@@ -11,7 +11,7 @@ export default function Download() {
             <p className="text">下記リンクをクリックして，ダウンロード可能です．個別の記載があるものを除き，無許可での転載・改変は禁じます．転載・改変を希望される場合は，Contactsに記載の連絡先までご連絡ください．尚，正当な手続きを経て転載される場合も，出典を明記してご利用ください．</p>
             <a href="./wingLogo.png" download='wingLogo.png' target='_blank'>このサイトのロゴ（PNG）</a><br />
             <a href="./wingLogo.svg" download='wingLogo.svg' target='_blank'>このサイトのロゴ（SVG）</a><br />
-            <a href="https://github.com/te-wing/wing-site-react" target='_blank'>このサイトのソースコード</a>
+            <a href="https://github.com/te-wing/wing-site-react" target='_blank'>このサイトのソースコード（GitHub）</a>
         </>
     );
 }
