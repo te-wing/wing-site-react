d25c48da1385d53d707528e94a29537cac8e700c
Tsubasa <wing@me.wing.osaka>
2025-04-02 17:03:46 +0900

    vike

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import vike from 'vike/plugin'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    vike({
+      pages: 'src/pages', // ページのディレクトリを指定
+      prerender: true, // プリレンダリングを有効化
+    })
+  ],
 })
